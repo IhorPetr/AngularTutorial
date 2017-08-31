be5e7d8c8a527a3229e2d4ccfc2bb7c5508fcd66
IhorPetr <vep2014@ukr.net>
2017-08-31 22:28:13 +0300

    clear project

---
+++ src/app/Hero.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by Igor on 31.08.2017.
+ */
+export class Hero {
+    id: number;
+    name: string;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import  {Hero} from './Hero';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello {{hero.id}}</h1>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent
+{
+  public hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
