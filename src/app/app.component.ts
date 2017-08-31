23065201b70511c788e3ee1f6e3317da76ef4071
IhorPetr <vep2014@ukr.net>
2017-09-01 00:10:52 +0300

    Master/Detail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,31 @@ import  {Hero} from './Hero';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{hero.id}}</h1>`,
+  templateUrl:'./app.component.html',
+  styleUrls:['./app.component.css']
 })
 export class AppComponent
 {
   public hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  id: 1,
+  name: 'Windstorm'
+};
+  public heroes=HEROES;
+  public selectedHero:Hero;
+  public onSelected(heros:Hero): void
+  {
+    this.selectedHero=heros;
+  }
 }
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
