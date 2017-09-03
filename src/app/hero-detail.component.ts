d5a04a45e6956c0b1a199ca93d9ad176d9f3b864
IhorPetr <vep2014@ukr.net>
2017-09-03 13:40:06 +0300

    Add multiple Component

---
+++ src/app/hero-detail.component.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by Igor on 03.09.2017.
+ */
+import {Component,Input} from '@angular/core';
+import {Hero} from './Hero';
+
+@Component(
+    {
+        selector:'hero-detail',
+        templateUrl:'./hero-detail.component.html'
+    }
+)
+export class HeroDetailComponent
+{
+      @Input() hero:Hero;
+}
